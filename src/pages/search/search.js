da91355fb48b7e9c2598b1b513ca4e2dc8ee0958
ruotingw <wruoting@gmail.com>
2020-03-30 00:17:44 -0400

    Delete button added

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -4,6 +4,12 @@ import Input from '@material-ui/core/Input';
 import Item from '../../components/Item';
 import { getRecipes } from '../../redux/API/search';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
 
 const ClickWrapper = styled.div``;
 
@@ -40,6 +46,18 @@ const Search = ({
         setListState([]);
     }
 
+    function deleteItem(outerIndex) {
+        let oldArray = selectedListState;
+        let newArray = oldArray.map((element, index) => {
+            if (index != outerIndex) {
+                return element;
+            }
+            return 0;
+        });
+        newArray.splice(outerIndex, 1);
+        setSelectedListState(newArray);
+    }
+
     return (
         <>
         <Input
@@ -61,6 +79,25 @@ const Search = ({
             :
             <></>
         }
+        <List dense={false}>
+        {
+            selectedListState.length > 0 ?
+                selectedListState.map((item, index)=> (
+                      <ListItem>
+                        <ListItemText
+                          primary={item.title}
+                        />
+                        <ListItemSecondaryAction>
+                          <IconButton edge="end" aria-label="delete">
+                            <DeleteIcon onClick={() => deleteItem(index)}/>
+                          </IconButton>
+                        </ListItemSecondaryAction>
+                      </ListItem>
+                ))
+            :
+            <></>
+        }
+        </List>
         </>
     );
 }
